34ce6068df6301566faf298f2d26d03e68e085ac
Sathishg090603 <sathishg090603@gmail.com>
2025-09-23 12:23:34 +0530

    changed anonymous to lambda functions

--- src/com/learning/ThreadsDemo/demo1.java
+++ src/com/learning/ThreadsDemo/demo1.java
@@ -5,38 +5,34 @@ public class demo1{
 	 
 	public static void main(String args[])
 	{
-		Runnable obj1=new Runnable() {  //anonymous class
-			
-			public void run()
-			{
+		Runnable obj1=()->     //lambda expression
+		{
 				for(int i=1;i<=10;i++)
 				{
 					System.out.println("hello");
 					try {
-						Thread.sleep(5);
+						Thread.sleep(10);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
-			}
 		};
-       Runnable obj2=new Runnable() {   //anonymous class
-			
-			public void run()
+		
+		
+      Runnable obj2=()->{   //lambda expression
+
+			for(int i=1;i<=10;i++)
 			{
-				for(int i=1;i<=10;i++)
-				{
-					System.out.println("hi");
-					try {
-						Thread.sleep(5);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				System.out.println("hi");
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
-		};
+      };
 		
 //		System.out.println("Priority of obj1:"+obj1.getPriority());
 //		obj2.setPriority(Thread.MAX_PRIORITY);
