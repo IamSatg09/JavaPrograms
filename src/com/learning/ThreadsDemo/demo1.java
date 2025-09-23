05add12c52fdf2d69dd747308f6a62c8570c1e2c
Sathishg090603 <sathishg090603@gmail.com>
2025-09-23 12:16:32 +0530

    changed Thread to Runnable & changed classes to anonoymous

--- src/com/learning/ThreadsDemo/demo1.java
+++ src/com/learning/ThreadsDemo/demo1.java
@@ -1,48 +1,50 @@
 package com.learning.ThreadsDemo;
 
- class A extends Thread{
-	 public void run()
-	 {
-		 for(int i=1;i<=100;i++)
-		 {
-		 System.out.println("hi");
-		 try {
-			Thread.sleep(5);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 }
-	 }
-}
- 
- class B extends Thread{
-	 public void run()
-	 {
-		 for(int i=1;i<=100;i++)
-		 {
-		 System.out.println("hello");
-		 try {
-			Thread.sleep(5);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 }
-	 }
- }
 public class demo1{
 
-	
+	 
 	public static void main(String args[])
 	{
-		A obj1=new A();
-		B obj2=new B();
+		Runnable obj1=new Runnable() {  //anonymous class
+			
+			public void run()
+			{
+				for(int i=1;i<=10;i++)
+				{
+					System.out.println("hello");
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+       Runnable obj2=new Runnable() {   //anonymous class
+			
+			public void run()
+			{
+				for(int i=1;i<=10;i++)
+				{
+					System.out.println("hi");
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		
 //		System.out.println("Priority of obj1:"+obj1.getPriority());
 //		obj2.setPriority(Thread.MAX_PRIORITY);
 		
-		obj1.start();
-		obj2.start();
+		Thread t1=new Thread(obj1);
+		Thread t2=new Thread(obj2);
+		t1.start();
+		t2.start();
 		
 	}
 }
